97498516602530b642b4f9287c2c66083debc532
unknown <ygor.aoliveira@senacsp.edu.br>
2025-01-15 01:28:48 -0300

    segundo commit

---
+++ 10-calculo_de_desconto.cs
@@ -0,0 +1,16 @@
+﻿       
+        Console.Write("Digite o valor da sua compra: R$ ");
+        double valorCompra = double.Parse(Console.ReadLine());
+
+        if (valorCompra > 100)
+        {
+            
+            double desconto = valorCompra * 0.10;
+            valorCompra -= desconto; 
+            Console.WriteLine($"Valor com desconto: R$ {valorCompra}");
+        }
+        else
+        {
+            Console.WriteLine($"Valor original: R$ {valorCompra}");
+        }
+    
